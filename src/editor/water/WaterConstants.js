318a96cc5d4ba165d4bc9ca0205e98c2ceab3969
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-07-27 10:24:11 +0400

    fix: make supported water kinds immutable

--- src/editor/water/WaterConstants.js
+++ src/editor/water/WaterConstants.js
@@ -11,9 +11,9 @@ export const WATER_BODY_ID_PROCEDURAL_OCEAN = 1;
 export const WATER_SAMPLE_FLAG_NONE = 0;
 export const WATER_SAMPLE_FLAG_INCOMPLETE_BED = 1;
 
-export const WATER_KINDS = Object.freeze(new Set([
+export const WATER_KINDS = Object.freeze([
   WATER_KIND_NONE,
   WATER_KIND_OCEAN,
   WATER_KIND_LAKE,
   WATER_KIND_RIVER,
-]));
+]);
