1494f95bf936862b4c20be506f60317a6067aec4
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-07-27 10:06:35 +0400

    feat: define water domain constants

---
+++ src/editor/water/WaterConstants.js
@@ -0,0 +1,19 @@
+export const WATER_DOMAIN_VERSION = 1;
+
+export const WATER_KIND_NONE = 0;
+export const WATER_KIND_OCEAN = 1;
+export const WATER_KIND_LAKE = 2;
+export const WATER_KIND_RIVER = 3;
+
+export const WATER_BODY_ID_NONE = 0;
+export const WATER_BODY_ID_PROCEDURAL_OCEAN = 1;
+
+export const WATER_SAMPLE_FLAG_NONE = 0;
+export const WATER_SAMPLE_FLAG_INCOMPLETE_BED = 1;
+
+export const WATER_KINDS = Object.freeze(new Set([
+  WATER_KIND_NONE,
+  WATER_KIND_OCEAN,
+  WATER_KIND_LAKE,
+  WATER_KIND_RIVER,
+]));
