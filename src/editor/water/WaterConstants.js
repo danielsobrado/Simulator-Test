28785dad5757d6246b62f9ef168192d1f89724aa
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-07-27 10:53:56 +0400

    feat: add stable river water body ids

--- src/editor/water/WaterConstants.js
+++ src/editor/water/WaterConstants.js
@@ -7,6 +7,7 @@ export const WATER_KIND_RIVER = 3;
 
 export const WATER_BODY_ID_NONE = 0;
 export const WATER_BODY_ID_PROCEDURAL_OCEAN = 1;
+export const WATER_BODY_ID_RIVER_BASE = 1024;
 
 export const WATER_SAMPLE_FLAG_NONE = 0;
 export const WATER_SAMPLE_FLAG_INCOMPLETE_BED = 1;
