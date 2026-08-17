11e43bfe28f9bc698af66a842f9d6c3a7d86d71d
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-18 03:18:23 +0400

    Scope hover bridge to the concrete editor

--- src/editor/ui/NaturalEditorHoverBridge.js
+++ src/editor/ui/NaturalEditorHoverBridge.js
@@ -1,15 +1,14 @@
-import { EditorController } from '../EditorController.js';
 import { TerrainAwareEditorController } from '../TerrainAwareEditorController.js';
 
 const PATCH_MARK = Symbol.for('drusniel.natural-editor-hover-bridge');
 const HOVER_EVENT = 'drusniel:natural-editor-hover';
 
-function patchHoverEmitter(prototype) {
-  if (Object.prototype.hasOwnProperty.call(prototype, PATCH_MARK)) return;
+function installHoverBridge() {
+  const prototype = TerrainAwareEditorController.prototype;
+  if (prototype[PATCH_MARK]) return;
   Object.defineProperty(prototype, PATCH_MARK, { value: true });
 
   const emitHover = prototype.emitHover;
-  if (typeof emitHover !== 'function') return;
   prototype.emitHover = function naturalEmitHover(cell) {
     const result = emitHover.call(this, cell);
     const tileId = cell ? this.tileMap.get(cell.x, cell.z) : null;
@@ -17,11 +16,14 @@ function patchHoverEmitter(prototype) {
     this.naturalLastContextTileId = tileId;
     const tile = tileId == null ? null : this.tileMap.getTileDefinition?.(tileId) ?? null;
     window.dispatchEvent(new CustomEvent(HOVER_EVENT, {
-      detail: tile ? Object.freeze({ tileId, tileKey: tile.key, terrainClass: tile.terrainClass }) : null,
+      detail: tile ? Object.freeze({
+        tileId,
+        tileKey: tile.key,
+        terrainClass: tile.terrainClass,
+      }) : null,
     }));
     return result;
   };
 }
 
-patchHoverEmitter(EditorController.prototype);
-patchHoverEmitter(TerrainAwareEditorController.prototype);
+installHoverBridge();
