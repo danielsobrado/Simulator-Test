bebe29431dfbf8778fc65be578d68e30a6ac99c3
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-17 16:03:35 +0400

    Enable rendered object thumbnails

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,6 +7,7 @@ import './main.js';
 import './editor/workshop/WorkshopRadialMenus.js';
 import './editor/ui/NaturalEditorUi.js';
 import './editor/ui/NaturalEditorRecommendations.js';
+import './editor/ui/NaturalObjectThumbnails.js';
 import './editor/ui/NaturalEditorProgressiveDisclosure.js';
 import './editor/ui/NaturalEditorUiBridge.js';
 import './editor/ui/NaturalEditorDrawerBehavior.js';
