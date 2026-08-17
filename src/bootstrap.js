672b178d80efa804412ae8fb08d611579b66845c
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-17 15:30:07 +0400

    Enable natural editor interaction shell

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,5 @@
 import './editor/lifecycle/installBfcacheRecovery.js';
+import './editor/ui/NaturalEditorInteractions.js';
 import './main.js';
 import './editor/workshop/WorkshopRadialMenus.js';
+import './editor/ui/NaturalEditorUi.js';
