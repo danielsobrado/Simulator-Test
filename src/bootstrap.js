9ed4aeb238e4868e96730137eb572694bcfd7867
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-17 15:44:33 +0400

    Enable terrain-aware natural UI recommendations

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,8 +1,10 @@
 import './editor/lifecycle/installBfcacheRecovery.js';
 import './editor/ui/NaturalEditorInteractions.js';
+import './editor/ui/NaturalEditorHoverBridge.js';
 import './editor/construction/ui/ConstructionThicknessGizmoInteraction.js';
 import './main.js';
 import './editor/workshop/WorkshopRadialMenus.js';
 import './editor/ui/NaturalEditorUi.js';
+import './editor/ui/NaturalEditorRecommendations.js';
 import './editor/ui/NaturalEditorProgressiveDisclosure.js';
 import './editor/ui/NaturalEditorUiBridge.js';
