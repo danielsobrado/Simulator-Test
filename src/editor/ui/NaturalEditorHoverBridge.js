d9ad2dc93cddc6017ed17b9f14ac304a7848cd2a
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-17 15:43:16 +0400

    Expose lightweight terrain context to natural UI

---
+++ src/editor/ui/NaturalEditorHoverBridge.js
@@ -0,0 +1,25 @@
+import { EditorController } from '../EditorController.js';
+
+const PATCH_MARK = Symbol.for('drusniel.natural-editor-hover-bridge');
+const HOVER_EVENT = 'drusniel:natural-editor-hover';
+
+function installHoverBridge() {
+  const prototype = EditorController.prototype;
+  if (prototype[PATCH_MARK]) return;
+  Object.defineProperty(prototype, PATCH_MARK, { value: true });
+
+  const emitHover = prototype.emitHover;
+  prototype.emitHover = function naturalEmitHover(cell) {
+    const result = emitHover.call(this, cell);
+    const tileId = cell ? this.tileMap.get(cell.x, cell.z) : null;
+    if (tileId === this.naturalLastContextTileId) return result;
+    this.naturalLastContextTileId = tileId;
+    const tile = tileId == null ? null : this.tileMap.getTileDefinition?.(tileId) ?? null;
+    window.dispatchEvent(new CustomEvent(HOVER_EVENT, {
+      detail: tile ? Object.freeze({ tileId, tileKey: tile.key, terrainClass: tile.terrainClass }) : null,
+    }));
+    return result;
+  };
+}
+
+installHoverBridge();
