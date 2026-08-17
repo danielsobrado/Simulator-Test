ba8956c973774806b9004d9838e4b5b96554bbaa
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-17 15:41:33 +0400

    Enable direct wall thickness editing

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,6 @@
 import './editor/lifecycle/installBfcacheRecovery.js';
 import './editor/ui/NaturalEditorInteractions.js';
+import './editor/construction/ui/ConstructionThicknessGizmoInteraction.js';
 import './main.js';
 import './editor/workshop/WorkshopRadialMenus.js';
 import './editor/ui/NaturalEditorUi.js';
