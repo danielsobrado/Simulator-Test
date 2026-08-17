7481f53ac932b942d971dffa8dde295947a1513d
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-17 15:32:35 +0400

    Bridge natural editor shortcuts and history state

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,3 +4,4 @@ import './main.js';
 import './editor/workshop/WorkshopRadialMenus.js';
 import './editor/ui/NaturalEditorUi.js';
 import './editor/ui/NaturalEditorProgressiveDisclosure.js';
+import './editor/ui/NaturalEditorUiBridge.js';
