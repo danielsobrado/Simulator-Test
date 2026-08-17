1bb7057c1712759fcb691a6c7e29c9122eac867c
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-18 02:56:42 +0400

    Wire multi-selection context UI

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -14,6 +14,7 @@ import './editor/ui/NaturalEditorProgressiveDisclosure.js';
 import './editor/ui/NaturalEditorUiBridge.js';
 import './editor/ui/NaturalEditorDrawerBehavior.js';
 import './editor/ui/NaturalConstructionContextToolbar.js';
+import './editor/ui/NaturalMultiSelectionUi.js';
 import './editor/ui/NaturalEditorContextPriority.js';
 import './editor/ui/NaturalPlayerSettings.js';
 import './editor/ui/NaturalEditorAudio.js';
