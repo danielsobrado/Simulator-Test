5cbd841eee0904b19e5fc06c0f825c57855445a9
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-18 03:23:46 +0400

    Install hover context per controller instance

--- src/editor/ui/NaturalEditorHoverBridge.js
+++ src/editor/ui/NaturalEditorHoverBridge.js
@@ -1,20 +1,18 @@
-import { TerrainAwareEditorController } from '../TerrainAwareEditorController.js';
-
-const PATCH_MARK = Symbol.for('drusniel.natural-editor-hover-bridge');
 const HOVER_EVENT = 'drusniel:natural-editor-hover';
 
-function installHoverBridge() {
-  const prototype = TerrainAwareEditorController.prototype;
-  if (prototype[PATCH_MARK]) return;
-  Object.defineProperty(prototype, PATCH_MARK, { value: true });
+export function installNaturalEditorHoverBridge(controller) {
+  if (controller.naturalEditorHoverBridge) return controller.naturalEditorHoverBridge;
+  const emitHover = controller.emitHover.bind(controller);
+  let lastTileId = null;
 
-  const emitHover = prototype.emitHover;
-  prototype.emitHover = function naturalEmitHover(cell) {
-    const result = emitHover.call(this, cell);
-    const tileId = cell ? this.tileMap.get(cell.x, cell.z) : null;
-    if (tileId === this.naturalLastContextTileId) return result;
-    this.naturalLastContextTileId = tileId;
-    const tile = tileId == null ? null : this.tileMap.getTileDefinition?.(tileId) ?? null;
+  controller.emitHover = (cell) => {
+    const result = emitHover(cell);
+    const tileId = cell ? controller.tileMap.get(cell.x, cell.z) : null;
+    if (tileId === lastTileId) return result;
+    lastTileId = tileId;
+    const tile = tileId == null
+      ? null
+      : controller.tileMap.getTileDefinition?.(tileId) ?? null;
     window.dispatchEvent(new CustomEvent(HOVER_EVENT, {
       detail: tile ? Object.freeze({
         tileId,
@@ -24,6 +22,12 @@ function installHoverBridge() {
     }));
     return result;
   };
-}
 
-installHoverBridge();
+  const integration = {
+    dispose() {
+      controller.naturalEditorHoverBridge = null;
+    },
+  };
+  controller.naturalEditorHoverBridge = integration;
+  return integration;
+}
