8e309e986c4dcc5e7d8bfcddb8c039c105625412
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-18 09:37:26 +0400

    Keep workshop material fixtures structurally valid

--- test/workshop-material-config.test.js
+++ test/workshop-material-config.test.js
@@ -10,6 +10,7 @@ import {
 function pngDataUrl(extraBytes = 0) {
   const header = Buffer.alloc(24);
   header.set([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]);
+  header.writeUInt32BE(13, 8);
   header.write('IHDR', 12, 'ascii');
   header.writeUInt32BE(1, 16);
   header.writeUInt32BE(1, 20);
